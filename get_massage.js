4da07882917a6cfddb43c645bac59525daa89285
kuroge_ <115858350+kurogee@users.noreply.github.com>
2023-04-02 14:54:22 +0900

    Update get_massage.js

--- get_massage.js
+++ get_massage.js
@@ -1,21 +1,18 @@
 async function get_mes() {
     document.getElementById("status").innerHTML = "お待ちください...";
 
-    /*
-    if (document.cookie.split(";")[0].split("=")[1] == "yes") {
+    if (document.cookie != "") {
         document.getElementById("status").innerHTML = "12時間に取得できるメッセージの最大は5通です。12時間後にまた来てください。";
-        return;
-    } else if (document.cookie == "") {
         localStorage.setItem("count", 0);
-    }*/
+        return;
+    }
 
     if (document.getElementById("user").value != "") {
-        /*
-        if (localStorage.getItem("count") != null && parseInt(localStorage.getItem("count")) >= 6) {
+        if (localStorage.getItem("count") != null && parseInt(localStorage.getItem("count")) >= 5) {
             document.cookie = "seigen=yes; max-age=43200";
             document.getElementById("status").innerHTML = "12時間に取得できるメッセージの最大は5通です。12時間後にまた来てください。";
             return;
-        }*/
+        }
 
         const response = await fetch(
             "https://script.google.com/macros/s/AKfycbyZUMF4F932tAg6ztXMJ95nIQrb4tteMVne19MitpmOlphucLaMx1ChqzB5jRSBW4VFQg/exec",
@@ -43,12 +40,11 @@ async function get_mes() {
 
         document.getElementById("status").innerHTML = "";
 
-        /*
         if (localStorage.getItem("count") != null) {
             localStorage.setItem("count", parseInt(localStorage.getItem("count"))+1);
         } else {
             localStorage.setItem("count", 1);
-        }*/
+        }
         
     } else {
         document.getElementById("status").innerHTML = "ユーザー名を入力してください！";
@@ -60,9 +56,13 @@ window.onload = () => {
     if (username != null) {
         document.getElementById("user").value = username;
     }
-    /*
+    
     const count = localStorage.getItem("count");
     if (count == null) {
         localStorage.setItem("count", 0);
-    }*/
+    }
+
+    if (document.cookie == "") {
+        localStorage.setItem("count", 0);
+    }
 }
