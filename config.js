fe3850f684eb5602654ea8d46d07f687997d7a72
kuroge_ <115858350+kurogee@users.noreply.github.com>
2023-05-17 19:03:03 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const key = {
+    message_fetch_url : "https://script.google.com/macros/s/AKfycbyupdYFWpSQyp-nbizVhpTbGzgp8JxTvyfnEdo9lOf8oP0Io88zCs-R9ZF0RRTugVcPLw/exec",
+    point_fetch_url : "https://script.google.com/macros/s/AKfycbxd3mqvKtBJn95__0IvnnV1giGUwfl_Vj0ZTWx7x-vvfUVLvIUgVoyW6VRvwKQwOOPkfA/exec"
+};
