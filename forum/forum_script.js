1f2ebe59db60140182f82716e5a96781f27e0490
kurogee <taiti.nakaya.2009@gmail.com>
2023-10-27 18:48:23 +0900

    掲示板制作（メッセージの送信はできるようになった！）

--- forum/forum_script.js
+++ forum/forum_script.js
@@ -1,4 +1,4 @@
-const url = "https://script.google.com/macros/s/AKfycbxieuyrIx10yjdtKzHO_KVrccGOHdRok4WN2GPtGnexIhCBkQQvpx0UnEnt8UEZHABdGQ/exec";
+const url = "https://script.google.com/macros/s/AKfycbw_HPP9vJ_0BqBFpxd00jZHD2SPubpMggk04HOzR-E4Zmc336q9mNkYn8xZx8iQGch7Jw/exec";
 
 async function getip() {
     const res = await fetch('https://ipinfo.io?callback').then(res => res.json()).then(json => json.ip);
--- forum/forums/common.js
+++ forum/forums/common.js
@@ -1,9 +1,57 @@
-const url = "https://script.google.com/macros/s/AKfycbxieuyrIx10yjdtKzHO_KVrccGOHdRok4WN2GPtGnexIhCBkQQvpx0UnEnt8UEZHABdGQ/exec";
+// https://script.google.com/macros/s/AKfycbw_HPP9vJ_0BqBFpxd00jZHD2SPubpMggk04HOzR-E4Zmc336q9mNkYn8xZx8iQGch7Jw/exec
 
 async function getip() {
     const res = await fetch('https://ipinfo.io?callback').then(res => res.json()).then(json => json.ip);
     console.log(res);
     return res;
+}
+
+async function get_messages(name="main") {
+    const response = await fetch(
+        url,
+        {
+            method : "POST",
+            body : JSON.stringify({
+                "request" : "get_comment",
+                "whereToGet" : name
+            })
+        }
+    )
+    .then(response => response.text())
+    .then(data => JSON.parse(data))
+    .catch(_ => { return "no" });
+
+    if (response == "no") {
+        return "no";
+    } else {
+        const messages_box = document.getElementById("messages_box");
+        let result = response.result.filter(_ => { return _.message == "" ? undefined : _ });
+
+        if (result.indexOf(undefined) != -1) {
+            result = {"parentID" : "", "messageID": "", "username" : "", "message" : "メッセージなし", "date" : ""};
+        }
+
+        messages_box.innerHTML = "<br>";
+        for (const message of result) {
+            console.log(message);
+            if (message.parentID != "") {
+                document.getElementById("ID" + message.parentID).innerHTML += `
+                    <div class="message reply">
+                        <p id="username">${message.username}<small id="date">　${message.date}</small></p>
+                        <p id="text">${message.message}</p>
+                    </div>`;
+            } else {
+                messages_box.innerHTML += `
+                    <div class="message" id="ID${message.messageID}">
+                        <p id="username">${message.username}<small id="date">　${message.date}</small></p>
+                        <p id="text">${message.message}</p>
+                    </div>
+                    <br>`;
+            }
+            
+        }
+        messages_box.innerHTML += "";
+    }
 }
 
 async function send_new_message(name="main") {
@@ -13,8 +61,8 @@ async function send_new_message(name="main") {
     const data = {
         "request" : "send_new_comment",
         "whereToSend" : name,
-        "username" : document.getElementById("username").value,
-        "message" : document.getElementById("message").value,
+        "username" : document.getElementById("username_box").value,
+        "message" : document.getElementById("message_box").value,
         "date" : date.getFullYear()
                 + '/' + ('0' + (date.getMonth() + 1)).slice(-2)
                 + '/' + ('0' + date.getDate()).slice(-2)
@@ -34,15 +82,20 @@ async function send_new_message(name="main") {
     )
     .then(response => response.text())
     .then(data => JSON.parse(data))
-    .catch(_ => { return "no" });
+    .catch(_ => { console.error(_); return "no" });
 
-    if (response == "ok") {
+    if (response.result == "ok") {
         status.innerText = "送信しました";
+        document.getElementById("username_box").value = "";
+        document.getElementById("message_box").value = "";
+        get_messages(document.getElementById("this_forum_name").value);
     } else {
         status.innerText = "送信に失敗しました";
     }
 }
 
-async function get_messages(name="main") {
-    
+onload = async () => {
+    if (location.href.match("forum") != null) {
+        get_messages(document.getElementById("this_forum_name").value);
+    }
 }
