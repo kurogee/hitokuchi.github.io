6d00093e3c98765b34fbc1d2e49c6644cb076476
kurogee <taiti.nakaya.2009@gmail.com>
2023-11-01 17:19:17 +0900

    板の追加や微量な修正

--- forum/forums/common.js
+++ forum/forums/common.js
@@ -1,4 +1,4 @@
-// https://script.google.com/macros/s/AKfycbwLj8hFgrGT6kR4YHFgI7mcZK-GENhNQqk7AMGw0SxmOtY2g96X5f0g2xmYQ6qxzMo0WA/exec
+// https://script.google.com/macros/s/AKfycbwwdm4Cr3AzXw96KsbKnZixvxNSOMUXcM_Qf9RK_Em4dDUNyYBc64j2HQVG5MSsdQDWMg/exec
 
 async function getip() {
     const res = await fetch('https://ipinfo.io?callback').then(res => res.json()).then(json => json.ip);
@@ -59,10 +59,6 @@ async function get_messages(name="main") {
         const messages_box = document.getElementById("messages_box");
         let result = response.result.filter(_ => { return _.message == "" ? undefined : _ });
 
-        /*if (result.indexOf(undefined) != -1) {
-            result = {"parentID" : "", "messageID": "", "username" : "", "message" : "メッセージなし", "date" : ""};
-        }*/
-
         messages_box.innerHTML = "<br>";
 
         for (const message of result) {
@@ -118,7 +114,6 @@ async function send_reply_message(name="main") {
 
     if (response.result == "ok") {
         status.innerText = "送信しました";
-        document.getElementById("username_box").value = "";
         document.getElementById("message_box").value = "";
         document.getElementById("reply_to").value = "";
         get_messages(document.getElementById("this_forum_name").value);
@@ -157,7 +152,6 @@ async function send_new_message(name="main") {
 
     if (response.result == "ok") {
         status.innerText = "送信しました";
-        document.getElementById("username_box").value = "";
         document.getElementById("message_box").value = "";
         get_messages(document.getElementById("this_forum_name").value);
     } else {
