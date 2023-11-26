07d5b4750e2bc243e8a4ec3dbc7d1b419157ee4b
kurogee <taiti.nakaya.2009@gmail.com>
2023-11-26 11:33:52 +0900

    不要部分を削除2

--- forum/forums/common.js
+++ forum/forums/common.js
@@ -1,5 +1,3 @@
-// https://script.google.com/macros/s/AKfycbwwdm4Cr3AzXw96KsbKnZixvxNSOMUXcM_Qf9RK_Em4dDUNyYBc64j2HQVG5MSsdQDWMg/exec
-
 async function getip() {
     const res = await fetch('https://ipinfo.io?callback').then(res => res.json()).then(json => json.ip);
     console.log("IP: " + res);
